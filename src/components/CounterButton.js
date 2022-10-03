e05e65d90ae7e69ddd1716a55f6f94ca0d7fdf95
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-03 13:23:39 -0600

    create and export default the init functional components

---
+++ src/components/CounterButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CounterButton(props) {
+  return (
+    <button className="CounterButton">
+      You clicked me X amount of times
+    </button>
+  );
+};
+
+
+
+export default CounterButton;
