0bb296f1fcbd062450a9091f44629c397e8e502f
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-03 13:28:26 -0600

    import components into App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+// CSS Files
 import './App.css';
+// Functional Components
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
     </div>
   );
 }
